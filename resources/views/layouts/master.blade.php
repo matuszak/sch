20fbd68ec43e4f547ee7ff291353b7efde95f937
Lailson Matuszak <lailsonbr@gmail.com>
2015-12-04 11:29:21 +0000

    Inicio bancos modelados, porem não prontos nem executados!

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,6 +1,9 @@
 <html>
     <head>
         <title>SCHOOLSys @yield('SCHOOLSys')</title>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
     </head>
     <body>
         @section('sidebar')
